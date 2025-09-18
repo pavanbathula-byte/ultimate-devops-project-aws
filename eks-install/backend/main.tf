ef71e603a48379d20f9cb8dead1f8e54d4f4bd50
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-18 19:02:27 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -10,6 +10,38 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# IAM role for EKS cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach required policies to the role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "eks" {
   name     = "demo-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
