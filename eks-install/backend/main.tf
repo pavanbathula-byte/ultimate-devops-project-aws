444e180122c2f60147522167e4957543601c0284
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-18 21:21:18 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -23,6 +23,38 @@ resource "aws_eks_node_group" "example" {
   }
 }
 
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach required managed policies
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
 # IAM role for EKS cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
