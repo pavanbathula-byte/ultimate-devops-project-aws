6b66a5d248402cf8ae2a4d4a8510d24f953896a2
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-18 19:23:14 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -52,6 +52,22 @@ resource "aws_eks_cluster" "eks" {
   }
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+
 terraform {
   required_providers {
     aws = {
