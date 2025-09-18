d82cb729d047e3b55ab8f39f130c0e1dec4e8fac
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-18 19:59:19 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -59,13 +59,13 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-west-2"
 }
 
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-west-2"
 }
 
 terraform {
