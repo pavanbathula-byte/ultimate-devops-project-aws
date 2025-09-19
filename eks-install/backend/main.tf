8a082aff93f1101e0c7062ae9c95a5b53c29acf0
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-19 18:36:31 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -155,6 +155,37 @@ resource "aws_subnet" "public2" {
   availability_zone = "us-west-2b"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eks_node_group" "example" {
+  ...
+  resources {
+    security_group_ids = [aws_security_group.node_sg.id]
+  }
+}
+
 terraform {
   required_providers {
     aws = {
