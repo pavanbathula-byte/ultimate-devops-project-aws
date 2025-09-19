75faee7be7c6471876a33df0a63e5a6bf78f06d0
pavanbathula-byte <pavanbathula225@gmail.com>
2025-09-19 19:11:05 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -147,12 +147,14 @@ resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
+ map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
+ map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
